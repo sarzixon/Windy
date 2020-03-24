12ff0587a423306a820d7f690c373db85090bff7
Michał Sarzała <sarzal666@gmail.com>
2020-03-24 19:03:56 +0100

    fixed https problem

--- src/js/apiCall.js
+++ src/js/apiCall.js
@@ -10,7 +10,7 @@
      }
      async apiCall() {
          try {
-            this.response = await axios.get(`http://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/forecast?q=${this.query},pl&units=metric&appid=${base.APIkey}`);
+            this.response = await axios.get(`https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/forecast?q=${this.query},pl&units=metric&appid=${base.APIkey}`);
          } catch (error) {
              console.log(error);
          }
